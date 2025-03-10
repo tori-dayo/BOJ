ae1d2c9f598bef8f2f6de7d3ddc1ad8e8c066190
Tori <45654620+tori-dayo@users.noreply.github.com>
2025-03-10 21:07:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 36376 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+n,k = map(int,input().split())
+visited = [False] * 100001
+def bfs(v):
+    queue = deque([(v,0)])
+    visited[v] = True
+    while queue:
+        x,time = queue.popleft()
+        if x == k:
+            print(time)
+            exit()
+        if x-1>=0 and not visited[x-1]:
+            visited[x-1] = True
+            queue.append((x-1,time+1))
+        if x+1<=100000 and not visited[x+1]:
+            visited[x+1] = True
+            queue.append((x+1,time+1))
+        if 2*x <= 100000 and not visited[2*x]:
+            visited[2*x] = True
+            queue.append((2*x,time+1))
+
+if n == k:
+    print(0)
+else:
+    print(bfs(n)+1)
